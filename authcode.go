e19207c8de4358e08834d4cdc6a9948b92707145
ziyouzy <603708556@qq.com>
2021-05-07 23:46:12 -0700

    20210508-ee逻辑已经重构完成,漏了authcode，已补全

--- authcode.go
+++ authcode.go
@@ -75,56 +75,48 @@ func (p *AuthCode)Construct(AuthCodeConfigAbs Config) error{
 	if c.UniqueId == "" {
 		return errors.New(
 			fmt.Sprintf("[river-node type:%s] init error, uniqueId is nil",
-			p.Name())
-		)
+			p.Name()))
 	}
 
 	if c.Events == nil || c.Errors == nil || c.Raws == nil{
 		return errors.New(
 			fmt.Sprintf("[uid:%s] init error, Events or Errors or Raws is nil",
-			c.UniqueId)
-		)
+			c.UniqueId))
 	}
 
 	if c.News_Encode != nil || c.News_Decode != nil{
 		return errors.New(
 			fmt.Sprintf("[uid:%s] init error, News_Encode and News_Decode must nil",
-			c.UniqueId)
-		)
+			c.UniqueId))
 	}
 
 	if c.Mode != ENCODE&&c.Mode != DECODE {
 		return errors.New(
-			fmt.Sprintf("[uid:%s] init error, unknown mode",c.UniqueId)
-		)
+			fmt.Sprintf("[uid:%s] init error, unknown mode",c.UniqueId))
 	}
 
 	if c.AuthCode_Key ==""{
 		return errors.New(
-			fmt.Sprintf("[uid:%s] init error, Salt is nil",c.UniqueId)
-		) 
+			fmt.Sprintf("[uid:%s] init error, Salt is nil",c.UniqueId)) 
 	}
 
 	//AuthCode_ExpirySec与AuthCode_DynamicKeyLen都可以为0，但是不推荐
 	
 	if c.Mode ==DECODE && (c.Limit_Decode ==0){
 		return errors.New(
-			fmt.Sprintf("[decode-uid:%s] init error, Limit_Decode is nil",c.UniqueId)
-		) 
+			fmt.Sprintf("[decode-uid:%s] init error, Limit_Decode is nil",c.UniqueId)) 
 	}
 
 	if c.Mode ==ENCODE && (c.Limit_Encode ==0){
 		return errors.New(
-			fmt.Sprintf("[encode-uid:%s] init error, Limit_Encode is nil",c.UniqueId)
-		) 
+			fmt.Sprintf("[encode-uid:%s] init error, Limit_Encode is nil",c.UniqueId)) 
 	}
 
 	
 	p.config = c
 
 	p.authcode = go_authcode.New(
-		p.config.AuthCode_Key, p.config.AuthCode_DynamicKeyLen, p.config.AuthCode_ExpirySec
-	)
+		p.config.AuthCode_Key, p.config.AuthCode_DynamicKeyLen, p.config.AuthCode_ExpirySec)
 
 	p.warpError_Panich = fmt.Errorf("%w",NewEvent(AUTHCODE_PANICH, c.UniqueId, "", ""))
 
@@ -145,14 +137,12 @@ func (p *AuthCode)Run(){
 		modeStr ="authcode为加密模式，将加密后的数据注入News_Encode管道"
 		p.config.Events <-NewEvent(
 			AUTHCODE_RUN,p.config.UniqueId,"",
-			fmt.Sprintf("[uid:%s;mode:%s]开始运行",p.config.UniqueId, modeStr)
-		)
+			fmt.Sprintf("[uid:%s;mode:%s]开始运行",p.config.UniqueId, modeStr))
 	}else if p.config.Mode == DECODE{
 		modeStr ="authcode为解密模式，将解密后的数据注入News_Decode管道"
 		p.config.Events <-NewEvent(
 			AUTHCODE_RUN,p.config.UniqueId,"",
-			fmt.Sprintf("[uid:%s;mode:%s]开始运行", p.config.UniqueId, modeStr)
-		)
+			fmt.Sprintf("[uid:%s;mode:%s]开始运行", p.config.UniqueId, modeStr))
 	}
 
 	switch p.config.Mode{
@@ -202,8 +192,7 @@ func (p *AuthCode)reactiveDestruct(){
 
 	p.config.Events <-NewEvent(
 		AUTHCODE_REACTIVE_DESTRUCT,p.config.UniqueId,"",
-		fmt.Sprintf("[uid:%s]触发了隐式析构方法",p.config.UniqueId)
-	)
+		fmt.Sprintf("[uid:%s]触发了隐式析构方法",p.config.UniqueId))
 }
 
 
@@ -217,8 +206,7 @@ func init() {
 	Register(AUTHCODE_NODE_NAME, NewAuthCode)
 	logger.Info(
 		fmt.Sprintf("预加载完成，[river-node type:%s]已预加载至package river_node.RNodes结构内",
-		AUTHCODE_NODE_NAME)
-	)
+		AUTHCODE_NODE_NAME))
 }
 
 /*------------以下是所需的功能方法-------------*/
@@ -232,19 +220,17 @@ func (p *AuthCode)encode(baits []byte){
 				"%v",NewEvent(AUTHCODE_ENCODE_FAIL, p.config.UniqueId, 
 				fmt.Sprintf("%x",baits), fmt.Sprintf("[uid:%s]连续%d次加密失败，"+
 				"已超过系统设定的最大次数，系统设定的最大连续失败次数为%d,[报错内容为%s]",
-				p.config.UniqueId, p.countor, p.config.Limit_Encode, err.Error()))
-			)
+				p.config.UniqueId, p.countor, p.config.Limit_Encode, err.Error())))
 
 			p.config.Errors <-p.warpError_Panich
 
 			p.countor =0
 		}else{
 			p.config.Errors <-fmt.Errorf(
-				"%v",NewError(AUTHCODE_ENCODE_FAIL, p.config.UniqueId, 
+				"%v",NewEvent(AUTHCODE_ENCODE_FAIL, p.config.UniqueId, 
 				fmt.Sprintf("%x",baits), fmt.Sprintf("[uid:%s]连续第%d次加密失败，"+
 				"当前系统设定的最大连续失败次数为%d,[报错内容为%s]",
-				p.config.UniqueId, p.countor, p.config.Limit_Encode, err.Error()))
-			)
+				p.config.UniqueId, p.countor, p.config.Limit_Encode, err.Error())))
 		}
 	}else{
 		p.config.News_Encode <- res
@@ -253,8 +239,7 @@ func (p *AuthCode)encode(baits []byte){
 			p.config.Events <-NewEvent(
 				AUTHCODE_ENCODE_RECOVERED, p.config.UniqueId, "", 
 				fmt.Sprintf("[uid:%s]已从第%d次加密失败中恢复，当前系统设定的最大失败次数为%d",
-				p.config.UniqueId, p.countor, p.config.Limit_Encode)
-			)
+				p.config.UniqueId, p.countor, p.config.Limit_Encode))
 
 			p.countor =0
 		}
@@ -271,18 +256,17 @@ func (p *AuthCode)decode(baits []byte){
 				"%v", NewEvent(AUTHCODE_DECODE_FAIL, p.config.UniqueId, 
 				fmt.Sprintf("%x",baits), fmt.Sprintf("[uid:%s]连续%d次解密失败，"+
 				"已超过系统设定的最大次数，系统设定的最大连续失败次数为%d,[报错内容为%s]",
-				p.config.UniqueId, p.countor,p.config.Limit_Decode,err.Error()))
-			)
+				p.config.UniqueId, p.countor,p.config.Limit_Decode,err.Error())))
 
-			p.config.Events <-p.warpError_Panich
+			p.config.Errors <-p.warpError_Panich
 
 			p.countor =0		
 		}else{
+
 			p.config.Errors <-fmt.Errorf(
 				"%v", NewEvent(AUTHCODE_DECODE_FAIL, p.config.UniqueId, fmt.Sprintf("%x",baits),
 				fmt.Sprintf("[uid:%s]连续%d次解密失败，当前系统设定的最大连续失败次数为%d,[报错内容为%s]",
-				p.config.UniqueId, p.countor, p.config.Limit_Decode,err.Error()))
-			)
+				p.config.UniqueId, p.countor, p.config.Limit_Decode,err.Error())))
 		}
 		
 	}else{
@@ -292,8 +276,7 @@ func (p *AuthCode)decode(baits []byte){
 			p.config.Events <-NewEvent(
 				AUTHCODE_DECODE_RECOVERED, p.config.UniqueId,"", 
 				fmt.Sprintf("[uid:%s]已从第%d次解密失败中恢复，当前系统设定的最大失败次数为%d",
-				p.config.UniqueId,p.countor,p.config.Limit_Decode)
-			)
+				p.config.UniqueId,p.countor,p.config.Limit_Decode))
 
 			p.countor =0
 		}
